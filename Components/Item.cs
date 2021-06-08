4c631d2ed92991874d8219a45b9ae68b72a3344b
QZoroark <quacmichael@gmail.com>
2021-06-08 17:01:18 +0700

    Add item initialization and aetherPhysics

--- Components/Item.cs
+++ Components/Item.cs
@@ -1,6 +1,4 @@
-﻿using System.Net.Mail;
-
-namespace TFT_Engine.Components
+﻿namespace TFT_Engine.Components
 {
     public class Item
     {
@@ -8,23 +6,8 @@ namespace TFT_Engine.Components
         public int id;
         public string name;
 
-        public Item(string name, int id)
-        {
-            this.name = name;
-            this.id = id;
-        }
-        /// <summary>
-        /// Makes item's stats change
-        /// </summary>
-        /// <remarks>
-        /// All hp increasing item must add both hp and maxHp
-        /// </remarks>
-        /// <param name="stats"></param>
-        public virtual void BaseStatsChanges(Statistics stats)
-        { }
         public virtual void OnStart()
         {
-            BaseStatsChanges(Holder.currentStats);
         }
 
         public virtual void OnTick()
